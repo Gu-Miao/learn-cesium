5f759448a99760ea443d0a9566143a73a83c12a0
Gu-Miao <gutianhuang@foxmail.com>
2021-08-19 18:07:24 +0800

    Modify viewFrom of CZML.

--- src/pages/czml/path.js
+++ src/pages/czml/path.js
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import Cesium from '@utils/cesium'
+import Cesium, { CesiumNavigation } from '@utils/cesium'
 import { Button } from 'antd'
 import dayjs from 'dayjs'
 
@@ -13,7 +13,7 @@ const duration = 20
 const count = 80
 let positions = []
 
-function createCZML(positions) {
+function createCZML(positions, viewFrom) {
   const start = dayjs()
   const end = start.clone().add(duration, 's')
 
@@ -60,6 +60,9 @@ function createCZML(positions) {
           rgba: [0, 0, 0, 255]
         }
       },
+      viewFrom: {
+        cartesian: viewFrom
+      },
       position: {
         epoch: startISO,
         cartographicDegrees: getCartographicDegrees(positions)
@@ -93,6 +96,8 @@ class Path extends Component {
       shouldAnimate: true
     })
 
+    new CesiumNavigation(this.viewer, {})
+
     this.scene = this.viewer.scene
     this.camera = this.scene.camera
 
@@ -110,6 +115,67 @@ class Path extends Component {
     this.flyTo()
 
     this.getPathPositions()
+  }
+  /**
+   * @param {Cesium.Cartesian3} prev
+   * @param {Cesium.Cartesian3} next
+   * @param {number} pitch
+   * @param {number} backward
+   * @returns
+   */
+  getXYZ = (prev, next, pitch = -45, backward = 1000) => {
+    const [p1, p2] = this.getLocalPositions(prev, next)
+    const heading = this.getHeadingDegree(p1.x, p1.y, p2.x, p2.y)
+
+    const xDiff = p2.x - p1.x
+    const yDiff = p2.y - p1.y
+
+    const x = backward * Math.abs(Math.sin(heading)) * (xDiff > 0 ? -1 : 1)
+    const y = backward * Math.abs(Math.cos(heading)) * (yDiff > 0 ? -1 : 1)
+    const z = backward / Math.abs(Math.tan(pitch))
+
+    return [x, y, z]
+  }
+  /**
+   * @param {Cesium.Cartesian3[]} prev Prev position lon lat height array.
+   */
+  getLocalPositions = (prev, next) => {
+    const localToWorldMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(prev)
+    const worldToLocalMatrix = Cesium.Matrix4.inverse(localToWorldMatrix, new Cesium.Matrix4())
+
+    const p1 = Cesium.Matrix4.multiplyByPoint(worldToLocalMatrix, prev, new Cesium.Cartesian3())
+
+    const p2 = Cesium.Matrix4.multiplyByPoint(worldToLocalMatrix, next, new Cesium.Cartesian3())
+
+    return [p1, p2]
+  }
+  getHeadingDegree = (x1, y1, x2, y2) => {
+    const dividend = x2 - x1
+    const divisor = y2 - y1
+
+    // If x2=x1, alpha could only be 0 or 180.
+    if (dividend === 0) {
+      return divisor >= 0 ? 0 : 180
+    }
+
+    // If y2=y1, alpha could only be 90 or -90.
+    if (divisor === 0) {
+      // There is no equal judgement due to it already be done above.
+      return dividend > 0 ? 90 : -90
+    }
+
+    // Calculate the alpha angle.
+    const tanAlpha = (x2 - x1) / (y2 - y1)
+    const alpha = (Math.atan(tanAlpha) / Math.PI) * 180
+
+    if (divisor > 0) {
+      return alpha
+    } else {
+      // If y2-y1<0, alpha is just the supplement of real angle.
+      const multiplier = x2 - x1 > 0 ? 1 : -1
+      const realAlpha = (180 - Math.abs(alpha)) * multiplier
+      return realAlpha
+    }
   }
   getPathPositions = async () => {
     const p = []
@@ -141,8 +207,13 @@ class Path extends Component {
   loadCZML = async toEnd => {
     this.clearPrevDataSource()
     const p = toEnd ? positions : [...positions].reverse()
+    const args = toEnd ? [start, end] : [end, start]
+    const viewFrom = this.getXYZ(...args)
+    console.log(viewFrom)
     try {
-      this.ds = await this.viewer.dataSources.add(Cesium.CzmlDataSource.load(createCZML(p)))
+      this.ds = await this.viewer.dataSources.add(
+        Cesium.CzmlDataSource.load(createCZML(p, viewFrom))
+      )
       const entity = this.ds.entities.getById('path')
       this.viewer.zoomTo(entity)
       this.viewer.trackedEntity = entity
