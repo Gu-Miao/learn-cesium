e2f33b2d22cdcc5071efbd1dd7f34c7eba29a2aa
Gu-Miao <1450206741@qq.com>
2021-09-28 22:38:48 +0800

    Remove useless codes.

--- src/utils/cesium.js
+++ src/utils/cesium.js
@@ -1,27 +1,5 @@
-import * as _Cesium from 'cesium'
+import * as Cesium from 'cesium'
 import CesiumNavigation from 'cesium-navigation-es6'
-import GmMaterialProperty, { source, logo } from './GmMaterialProperty'
-
-const Cesium = {
-  ..._Cesium,
-  GmMaterialProperty
-}
-
-Cesium.Material.GmType = 'Gm'
-Cesium.Material._materialCache.addMaterial(Cesium.Material.GmType, {
-  fabric: {
-    type: Cesium.Material.GmType,
-    source,
-    uniforms: {
-      color: new Cesium.Color(0.0, 0.0, 1.0, 0.5),
-      image: logo,
-      time: 20
-    }
-  },
-  translucent(material) {
-    return true
-  }
-})
 
 export { CesiumNavigation }
 export default Cesium
