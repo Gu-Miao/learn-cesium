a2261ca99a985ce3a6f773f25aa41e7492462f52
Gu-Miao <1450206741@qq.com>
2021-11-11 14:16:54 +0800

    Update Clamp To 3dtiles exmpales.

--- src/pages/Cesium3DTiles/ClampTo3DTiles.jsx
+++ src/pages/Cesium3DTiles/ClampTo3DTiles.jsx
@@ -1,21 +1,24 @@
 import { Component } from 'react'
-import Cesium from '@utils/cesium'
+import Cesium, { CesiumNavigation } from '@utils/cesium'
+import { Button } from 'antd'
 import czml from './ClampToGroundCZML'
 
 class AjustHeight extends Component {
-  async componentDidMount() {
-    const viewer = new Cesium.Viewer('stage', {
-      terrainProvider: Cesium.createWorldTerrain()
+  state = {
+    disabled: true
+  }
+  componentDidMount() {
+    this.viewer = new Cesium.Viewer('stage', {
+      terrainProvider: Cesium.createWorldTerrain(),
+      shouldAnimate: true
     })
 
-    const { scene, clock, dataSources } = viewer
+    CesiumNavigation(this.viewer, {})
+
+    const { scene, camera, screenSpaceEventHandler } = this.viewer
 
     scene.postProcessStages.fxaa.enabled = true
     scene.debugShowFramesPerSecond = true
-
-    const ds = await dataSources.add(Cesium.CzmlDataSource.load(czml))
-    const truck = ds.entities.getById('CesiumMilkTruck')
-    const positionProperty = truck.position
 
     const tileset = scene.primitives.add(
       new Cesium.Cesium3DTileset({
@@ -23,7 +26,7 @@ class AjustHeight extends Component {
       })
     )
 
-    viewer.camera.setView({
+    camera.setView({
       destination: new Cesium.Cartesian3(1216403.8845586285, -4736357.493351395, 4081299.715698949),
       orientation: new Cesium.HeadingPitchRoll(
         4.2892217081808806,
@@ -34,22 +37,50 @@ class AjustHeight extends Component {
     })
 
     if (scene.clampToHeightSupported) {
-      tileset.initialTilesLoaded.addEventListener(start)
+      tileset.initialTilesLoaded.addEventListener(() => this.setState({ disabled: false }))
     } else {
       window.alert('This browser does not support clampToHeight.')
     }
 
-    function start() {
-      clock.shouldAnimate = true
-      const objectsToExclude = [truck]
-      scene.postRender.addEventListener(function () {
-        const position = positionProperty.getValue(clock.currentTime)
-        truck.position = scene.clampToHeight(position, objectsToExclude)
-      })
+    screenSpaceEventHandler.setInputAction(movement => {
+      const pick = scene.pickPosition(movement.position)
+      const c = Cesium.Cartographic.fromCartesian(pick)
+
+      console.log(Cesium.Math.toDegrees(c.longitude))
+      console.log(Cesium.Math.toDegrees(c.latitude))
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
+  }
+  loadCZML = async () => {
+    const { scene, dataSources, clock } = this.viewer
+    if (this.ds) {
+      dataSources.remove(this.ds)
     }
+    this.ds = await dataSources.add(Cesium.CzmlDataSource.load(czml))
+    const truck = this.ds.entities.getById('CesiumMilkTruck')
+    const positionProperty = truck.position
+
+    const objectsToExclude = [truck]
+    const postRender = () => {
+      const position = positionProperty.getValue(clock.currentTime)
+      if (!position) {
+        scene.postRender.removeEventListener(postRender)
+        return
+      }
+      truck.position = scene.clampToHeight(position, objectsToExclude)
+    }
+    scene.postRender.addEventListener(postRender)
   }
   render() {
-    return <div id="stage"></div>
+    return (
+      <>
+        <div className="toolbar">
+          <Button type="primary" onClick={this.loadCZML} disabled={this.state.disabled}>
+            Load CZML
+          </Button>
+        </div>
+        <div id="stage"></div>
+      </>
+    )
   }
 }
 
--- src/pages/Cesium3DTiles/ClampToGroundCZML.js
+++ src/pages/Cesium3DTiles/ClampToGroundCZML.js
@@ -1,66 +1,149 @@
 import gltf from '@assets/CesiumMilkTruck.glb'
+import Cesium from '@utils/cesium'
+import dayjs from 'dayjs'
+
+const positions = [
+  [-75.59682640355302, 40.0377282591004, 30],
+  [-75.59721134830173, 40.037671211919225, 30],
+  [-75.59747878476885, 40.037898069427314, 30],
+  [-75.59700518759936, 40.03799476320532, 30]
+]
+
+const duration = 1 * 60
+const modelDefaultHeading = -90
+const turnDuration = 2
+let totalDistance = 0
+let time = 0
+const distances = []
+const orientations = []
+const cartographicDegrees = []
+const unitQuaternion = []
+
+for (let i = 1; i < positions.length; ++i) {
+  const prev = Cesium.Cartesian3.fromDegrees(...positions[i - 1])
+  const next = Cesium.Cartesian3.fromDegrees(...positions[i])
+  const distance = Cesium.Cartesian3.distance(prev, next)
+  totalDistance += distance
+  distances.push(distance)
+
+  const { prev: localPrev, next: localNext } = getLocalPositions(prev, next)
+  const heading = getHeadingDegree(localPrev, localNext)
+
+  const orientation = Cesium.Transforms.headingPitchRollQuaternion(
+    prev,
+    new Cesium.HeadingPitchRoll(
+      Cesium.Math.toRadians(heading + modelDefaultHeading),
+      Cesium.Math.toRadians(0),
+      Cesium.Math.toRadians(0)
+    )
+  )
+  orientations.push(orientation)
+}
+
+for (let i = 0; i < positions.length; i++) {
+  const { x, y, z, w } = orientations[i] || {}
+  if (i === 0) {
+    cartographicDegrees.push(0, ...positions[0])
+    unitQuaternion.push(0, x, y, z, w)
+    continue
+  }
+  time = time + (distances[i - 1] / totalDistance) * duration
+  cartographicDegrees.push(time, ...positions[i])
+  if (i === positions.length - 1) break
+  const { x: _x, y: _y, z: _z, w: _w } = orientations[i - 1]
+
+  unitQuaternion.push(time - turnDuration, _x, _y, _z, _w)
+  unitQuaternion.push(time, x, y, z, w)
+}
+
+const startTime = dayjs()
+const endTime = startTime.add(duration, 's')
+const startTimeISO = startTime.toISOString()
+const endTimeISO = endTime.toISOString()
 
 const czml = [
   {
     id: 'document',
     version: '1.0',
     clock: {
-      interval: '2018-07-19T15:18:00Z/2018-07-19T15:18:30Z',
-      currentTime: '2018-07-19T15:18:00Z',
-      multiplier: 5,
-      range: 'LOOP_STOP',
-      step: 'SYSTEM_CLOCK_MULTIPLIER'
+      interval: `${startTimeISO}/${endTimeISO}`,
+      currentTime: startTimeISO,
+      multiplier: 3,
+      range: 'UNBOUNDED'
     }
   },
   {
     id: 'CesiumMilkTruck',
+    availability: `${startTimeISO}/${endTimeISO}`,
     model: {
       gltf
     },
     position: {
-      interpolationAlgorithm: 'LINEAR',
-      forwardExtrapolationType: 'HOLD',
-      cartesian: [
-        '2018-07-19T15:18:00Z',
-        1216348.1632364073,
-        -4736348.958775471,
-        4081284.5528982095,
-        '2018-07-19T15:18:30Z',
-        1216369.1229444197,
-        -4736377.467107148,
-        4081240.888485707
-      ]
+      epoch: startTimeISO,
+      cartographicDegrees
     },
     orientation: {
-      unitQuaternion: [
-        0.3084011337938999, 0.3210181022701266, -0.45850421987074924, 0.7686388857813198
-      ]
-    }
-  },
-  {
-    id: 'Polyline',
-    polyline: {
-      positions: {
-        cartesian: [
-          1216348.1632364073, -4736348.958775471, 4081284.5528982095, 1216369.1229444197,
-          -4736377.467107148, 4081240.888485707
-        ]
-      },
-      material: {
-        polylineOutline: {
-          color: {
-            rgba: [255, 255, 0, 255]
-          },
-          outlineColor: {
-            rgba: [0, 0, 0, 255]
-          },
-          outlineWidth: 2
-        }
-      },
-      width: 10,
-      clampToGround: true
+      epoch: startTimeISO,
+      unitQuaternion
     }
   }
 ]
+
+/**
+ * @param {Cesium.Cartesian3} prev Prev Cartesian3 position.
+ * @param {Cesium.Cartesian3} next Bext Cartesian3 position.
+ */
+function getLocalPositions(prev, next) {
+  const localToWorldMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(prev)
+  const worldToLocalMatrix = Cesium.Matrix4.inverse(localToWorldMatrix, new Cesium.Matrix4())
+
+  const localPosition = Cesium.Matrix4.multiplyByPoint(
+    worldToLocalMatrix,
+    prev,
+    new Cesium.Cartesian3()
+  )
+  const newPosition = Cesium.Matrix4.multiplyByPoint(
+    worldToLocalMatrix,
+    next,
+    new Cesium.Cartesian3()
+  )
+
+  return {
+    prev: localPosition,
+    next: newPosition
+  }
+}
+
+function getHeadingDegree(prev, next) {
+  const { x: x1, y: y1 } = prev
+  const { x: x2, y: y2 } = next
+
+  const dividend = x2 - x1
+  const divisor = y2 - y1
+
+  // If x2=x1, α could only be 0 or 180.
+  if (dividend === 0) {
+    return divisor >= 0 ? 0 : 180
+  }
+
+  // If y2=y1, α could only be 90 or -90.
+  if (divisor === 0) {
+    // There is no equal judgement due to it already be done above.
+    return dividend > 0 ? 90 : -90
+  }
+
+  // Calculate the α angle.
+  const tanα = (x2 - x1) / (y2 - y1)
+  const α = (Math.atan(tanα) / Math.PI) * 180
+
+  if (divisor > 0) {
+    return α
+  } else {
+    // If y2-y1<0, α is just the supplement of real angle.
+    const multiplier = x2 - x1 > 0 ? 1 : -1
+    const realα = (180 - Math.abs(α)) * multiplier
+    return realα
+  }
+}
 
 export default czml
