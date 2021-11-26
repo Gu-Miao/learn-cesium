c26aa1a13391ef8153f76414d2d7f040442824de
Gu-Miao <1450206741@qq.com>
2021-11-26 11:38:46 +0800

    Update getWiderView function

--- src/pages/Positions/WiderView.jsx
+++ src/pages/Positions/WiderView.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Cesium, { CesiumNavigation } from '@utils/cesium'
+import Cesium, { CesiumNavigation, getWiderView } from '@utils/cesium'
 
 const startDegree = [117.128202, 36.209944]
 const endDegree = [117.109628, 36.257021]
@@ -10,35 +10,33 @@ const end = Cesium.Cartesian3.fromDegrees(...endDegree)
 const radii = new Cesium.Cartesian3(50, 50, 50)
 
 class WiderView extends Component {
+  state = {
+    pitch: -45,
+    backward: 500
+  }
   async componentDidMount() {
-    const viewer = new Cesium.Viewer('stage', {
-      terrainProvider: Cesium.createWorldTerrain(),
-      shouldAnimate: true
-    })
+    const viewer = new Cesium.Viewer('stage')
+    this.viewer = viewer
 
     CesiumNavigation(viewer, {})
 
-    const sampledData = await Cesium.sampleTerrainMostDetailed(viewer.terrainProvider, [
-      Cesium.Cartographic.fromCartesian(start),
-      Cesium.Cartographic.fromCartesian(end)
-    ])
-
+    // Add two ellipsoids
     viewer.entities.add({
-      position: Cesium.Cartographic.toCartesian(sampledData[0]),
+      position: start,
+      ellipsoid: {
+        radii,
+        material: Cesium.Color.fromBytes(255, 255, 255, 255)
+      }
+    })
+    viewer.entities.add({
+      position: end,
       ellipsoid: {
         radii,
         material: Cesium.Color.fromBytes(255, 255, 255, 255)
       }
     })
 
-    viewer.entities.add({
-      position: Cesium.Cartographic.toCartesian(sampledData[1]),
-      ellipsoid: {
-        radii,
-        material: Cesium.Color.fromBytes(255, 255, 255, 255)
-      }
-    })
-
+    // Add red ground line
     viewer.scene.groundPrimitives.add(
       new Cesium.GroundPolylinePrimitive({
         geometryInstances: new Cesium.GeometryInstance({
@@ -50,28 +48,47 @@ class WiderView extends Component {
       })
     )
 
-    const data = getWiderView(start, end)
+    this.getWiderView()
+  }
+  componentDidUpdate() {
+    this.getWiderView()
+  }
+  clear = () => {
+    const { viewer } = this
+    if (this.extendedLine) {
+      viewer.scene.groundPrimitives.remove(this.extendedLine)
+      viewer.entities.remove(this.groudPoint)
+      viewer.entities.remove(this.point)
+    }
+  }
+  getWiderView = () => {
+    this.clear()
 
-    viewer.scene.groundPrimitives.add(
-      new Cesium.GroundPolylinePrimitive({
-        geometryInstances: new Cesium.GeometryInstance({
-          geometry: new Cesium.GroundPolylineGeometry({
-            positions: [start, data.groundPosition],
-            width: 10
+    const { viewer } = this
+    const { pitch, backward } = this.state
+    const data = getWiderView(start, end, pitch, backward)
+
+    if (data.pitch !== -90) {
+      this.extendedLine = viewer.scene.groundPrimitives.add(
+        new Cesium.GroundPolylinePrimitive({
+          geometryInstances: new Cesium.GeometryInstance({
+            geometry: new Cesium.GroundPolylineGeometry({
+              positions: [start, data.groundPosition],
+              width: 10
+            })
           })
         })
+      )
+      this.groudPoint = viewer.entities.add({
+        position: data.groundPosition,
+        ellipsoid: {
+          radii,
+          material: Cesium.Color.fromBytes(255, 255, 255, 255)
+        }
       })
-    )
+    }
 
-    viewer.entities.add({
-      position: data.groundPosition,
-      ellipsoid: {
-        radii,
-        material: Cesium.Color.fromBytes(255, 255, 255, 255)
-      }
-    })
-
-    viewer.entities.add({
+    this.point = viewer.entities.add({
       position: data.position,
       ellipsoid: {
         radii,
@@ -89,96 +106,45 @@ class WiderView extends Component {
     })
   }
   render() {
-    return <div id="stage"></div>
-  }
-}
+    const { pitch, backward } = this.state
+    return (
+      <>
+        <div className="toolbar">
+          Pitch:{' '}
+          <input
+            type="number"
+            value={pitch}
+            onChange={e => this.setState({ pitch: parseFloat(e.target.value) })}
+            step={1}
+            min={-90}
+            max={0}
+          />
+          <br />
+          Backward:{' '}
+          <input
+            type="number"
+            value={backward}
+            onChange={e => this.setState({ backward: parseFloat(e.target.value) })}
+            step={100}
+            min={10}
+            max={10000}
+          />
+          <br />
+          <button
+            onClick={() => {
+              this.setState({
+                pitch: Math.random() * -90,
+                backward: Math.random() * 10000
+              })
+            }}
+          >
+            Random
+          </button>
+        </div>
 
-/**
- * @param {Cesium.Cartesian3} p1
- * @param  {...Cesium.Cartesian3} rest
- * @returns
- */
-function getLocalPositions(p1, ...rest) {
-  const localToWorldMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(p1)
-  const worldToLocalMatrix = Cesium.Matrix4.inverse(localToWorldMatrix, new Cesium.Matrix4())
-
-  const _p1 = Cesium.Matrix4.multiplyByPoint(worldToLocalMatrix, p1, new Cesium.Cartesian3())
-
-  const _rest = rest.map(p =>
-    Cesium.Matrix4.multiplyByPoint(worldToLocalMatrix, p, new Cesium.Cartesian3())
-  )
-
-  return {
-    positions: [_p1, ..._rest],
-    localToWorldMatrix,
-    worldToLocalMatrix
-  }
-}
-
-function getHeadingDegree(x1, y1, x2, y2) {
-  const dividend = x2 - x1
-  const divisor = y2 - y1
-
-  // If x2=x1, alpha could only be 0 or 180.
-  if (dividend === 0) {
-    return divisor >= 0 ? 0 : 180
-  }
-
-  // If y2=y1, alpha could only be 90 or -90.
-  if (divisor === 0) {
-    // There is no equal judgement due to it already be done above.
-    return dividend > 0 ? 90 : -90
-  }
-
-  // Calculate the alpha angle.
-  const tanAlpha = (x2 - x1) / (y2 - y1)
-  const alpha = (Math.atan(tanAlpha) / Math.PI) * 180
-
-  if (divisor > 0) {
-    return alpha
-  } else {
-    // If y2-y1<0, alpha is just the supplement of real angle.
-    const multiplier = x2 - x1 > 0 ? 1 : -1
-    const realAlpha = (180 - Math.abs(alpha)) * multiplier
-    return realAlpha
-  }
-}
-
-/**
- * @param {Cesium.Cartesian3} prev
- * @param {Cesium.Cartesian3} next
- * @param {number} pitch
- * @param {number} backward
- * @returns
- */
-function getWiderView(prev, next, pitch = -45, backward = 1000) {
-  const { positions, localToWorldMatrix } = getLocalPositions(prev, next)
-  const [p1, p2] = positions
-  const heading = getHeadingDegree(p1.x, p1.y, p2.x, p2.y)
-
-  const xDiff = p2.x - p1.x
-  const yDiff = p2.y - p1.y
-
-  const x = backward * Math.abs(Math.sin(Cesium.Math.toRadians(heading))) * (xDiff > 0 ? -1 : 1)
-  const y = backward * Math.abs(Math.cos(Cesium.Math.toRadians(heading))) * (yDiff > 0 ? -1 : 1)
-  const z = backward / Math.abs(Math.tan(Cesium.Math.toRadians(pitch)))
-
-  p1.x += x
-  p1.y += y
-
-  const gp1 = p1.clone()
-
-  p1.z += z
-
-  const _p1 = Cesium.Matrix4.multiplyByPoint(localToWorldMatrix, p1, new Cesium.Cartesian3())
-  const _gp1 = Cesium.Matrix4.multiplyByPoint(localToWorldMatrix, gp1, new Cesium.Cartesian3())
-
-  return {
-    position: _p1,
-    groundPosition: _gp1,
-    heading,
-    pitch,
-    roll: 0
+        <div id="stage"></div>
+      </>
+    )
   }
 }
 
--- src/utils/cesium.js
+++ src/utils/cesium.js
@@ -12,5 +12,140 @@ function setHeight(c, h) {
   return Cesium.Cartographic.toCartesian(_c)
 }
 
-export { CesiumNavigation, addHeight, setHeight }
+/**
+ * Get heading degree
+ * @param {Cesium.Cartesian3} prev Prev Cartesian3 position.
+ * @param {Cesium.Cartesian3} next Bext Cartesian3 position.
+ * @returns {number} Heading degree
+ */
+function getHeadingDegree(prev, next) {
+  const { x: x1, y: y1 } = prev
+  const { x: x2, y: y2 } = next
+
+  const dividend = x2 - x1
+  const divisor = y2 - y1
+
+  // If x2=x1, α could only be 0 or 180.
+  if (dividend === 0) {
+    return divisor >= 0 ? 0 : 180
+  }
+
+  // If y2=y1, α could only be 90 or -90.
+  if (divisor === 0) {
+    // There is no equal judgement due to it already be done above.
+    return dividend > 0 ? 90 : -90
+  }
+
+  // Calculate the α angle.
+  const tanα = (x2 - x1) / (y2 - y1)
+  const α = (Math.atan(tanα) / Math.PI) * 180
+
+  if (divisor > 0) {
+    return α
+  } else {
+    // If y2-y1<0, α is just the supplement of real angle.
+    const multiplier = x2 - x1 > 0 ? 1 : -1
+    const realα = (180 - Math.abs(α)) * multiplier
+    return realα
+  }
+}
+
+/**
+ * Get local positions.
+ * @param {Cesium.Cartesian3} prev Prev Cartesian3 position.
+ * @param {Cesium.Cartesian3} next Bext Cartesian3 position.
+ * @returns {{prev: Cesium.Cartesian3, next: Cesium.Cartesian3, localToWorldMatrix: Cesium.Matrix4, worldToLocalMatrix: Cesium.Matrix4}} Local positions
+ */
+function getLocalPositions(prev, next) {
+  const localToWorldMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(prev)
+  const worldToLocalMatrix = Cesium.Matrix4.inverse(localToWorldMatrix, new Cesium.Matrix4())
+
+  const localPosition = Cesium.Matrix4.multiplyByPoint(
+    worldToLocalMatrix,
+    prev,
+    new Cesium.Cartesian3()
+  )
+  const newPosition = Cesium.Matrix4.multiplyByPoint(
+    worldToLocalMatrix,
+    next,
+    new Cesium.Cartesian3()
+  )
+
+  return {
+    prev: localPosition,
+    next: newPosition,
+    localToWorldMatrix,
+    worldToLocalMatrix
+  }
+}
+
+/**
+ * @param {Cesium.Cartesian3} postion1
+ * @param {Cesium.Cartesian3} postion2
+ * @param {number} pitch
+ * @param {number} backward
+ * @returns
+ */
+function getWiderView(postion1, postion2, pitch = -45, backward = 600) {
+  const { prev, next, localToWorldMatrix } = getLocalPositions(postion1, postion2)
+  const heading = getHeadingDegree(prev, next)
+
+  if (pitch === -90) {
+    const _position = prev.clone()
+    _position.z += backward
+    const position = Cesium.Matrix4.multiplyByPoint(
+      localToWorldMatrix,
+      _position,
+      new Cesium.Cartesian3()
+    )
+    return {
+      position,
+      groundPosition: prev,
+      heading,
+      pitch,
+      roll: 0
+    }
+  }
+
+  const mx = heading > 0 ? -1 : 1
+  const my = Math.abs(heading) < 90 ? -1 : 1
+  const alpha = Math.abs(heading) <= 90 ? Math.abs(heading) : 180 - Math.abs(heading)
+  const alphaRadian = Cesium.Math.toRadians(alpha)
+  const theta = 90 + pitch
+  const thetaRadian = Cesium.Math.toRadians(theta)
+
+  const deltaX = backward * Math.sin(alphaRadian) * mx
+  const deltaY = backward * Math.cos(alphaRadian) * my
+  const deltaZ = backward / Math.tan(thetaRadian)
+
+  const _groundPosition = prev.clone()
+
+  _groundPosition.x += deltaX
+  _groundPosition.y += deltaY
+
+  const _position = _groundPosition.clone()
+
+  _position.z += deltaZ
+
+  const position = Cesium.Matrix4.multiplyByPoint(
+    localToWorldMatrix,
+    _position,
+    new Cesium.Cartesian3()
+  )
+  const groundPosition = Cesium.Matrix4.multiplyByPoint(
+    localToWorldMatrix,
+    _groundPosition,
+    new Cesium.Cartesian3()
+  )
+
+  return {
+    position,
+    groundPosition,
+    heading,
+    pitch,
+    roll: 0
+  }
+}
+
+export { CesiumNavigation, addHeight, setHeight, getHeadingDegree, getLocalPositions, getWiderView }
 export default Cesium
