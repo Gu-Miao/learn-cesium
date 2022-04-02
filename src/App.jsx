822e7b50e551c7228610bc28a41d3682c93a962b
Gu-Miao <1450206741@qq.com>
2022-04-02 15:16:03 +0800

    update packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route, useNavigate, BrowserRouter } from 'react-router-dom'
 import { Container, Header, Navbar, Content, Loader, Footer } from 'rsuite'
 import { allCases } from './cases/cases'
 
@@ -16,34 +16,42 @@ function Loading() {
   )
 }
 
-function App() {
+function Brand() {
   const navigate = useNavigate()
   return (
-    <Container className="App">
-      <Header>
-        <Navbar appearance="inverse">
-          <Navbar.Brand onClick={() => navigate('/')}>
-            <span className="title">Learn Cesium</span>
-          </Navbar.Brand>
-        </Navbar>
-      </Header>
-      <Content id="stage">
-        <Suspense fallback={<Loading />}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/viewer">
-              {allCases.map(item => {
-                const { index, path, component } = item
-                const Component = lazy(() => import(`@/cases/${component}`))
-                return <Route index={index} key={component} path={path} element={<Component />} />
-              })}
-            </Route>
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </Suspense>
-      </Content>
-      <Footer className="footer">Made by Gu-Miao 💖</Footer>
-    </Container>
+    <Navbar.Brand onClick={() => navigate('/')}>
+      <span className="title">Learn Cesium</span>
+    </Navbar.Brand>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Container className="App">
+        <Header>
+          <Navbar appearance="inverse">
+            <Brand />
+          </Navbar>
+        </Header>
+        <Content id="stage">
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/viewer">
+                {allCases.map(item => {
+                  const { index, path, component } = item
+                  const Component = lazy(() => import(`@/cases/${component}`))
+                  return <Route index={index} key={component} path={path} element={<Component />} />
+                })}
+              </Route>
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </Suspense>
+        </Content>
+        <Footer className="footer">Made by Gu-Miao 💖</Footer>
+      </Container>
+    </BrowserRouter>
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,12 @@
-import { StrictMode } from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import App from './App'
+import { createRoot } from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
+import App from './App'
 
 import './index.less'
 
 window.CESIUM_BASE_URL = process.env.PUBLIC_URL
 
-render(
-  <StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <App />
-    </Router>
-  </StrictMode>,
-  document.getElementById('root')
-)
+createRoot(document.getElementById('root')).render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
