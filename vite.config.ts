07588356c58ce8017c83d8d2695a53bcb8daab72
Gu-Miao <1450206741@qq.com>
2023-03-02 14:08:42 +0800

    Add html plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteStaticCopy as copy } from 'vite-plugin-static-copy'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
@@ -20,7 +21,8 @@ export default defineConfig(() => ({
         }
       ]
     }),
-    vue()
+    vue(),
+    createHtmlPlugin({ minify: true })
   ],
   resolve: {
     alias: {
